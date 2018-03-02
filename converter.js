c33ce1c2425addb2ca2f57ef15625d148e90937c
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-03-02 15:12:07 -0600

    got the conversion to work

--- converter.js
+++ converter.js
@@ -13,34 +13,59 @@ console.log("converter.js");
 - [ ] For any other temperature, the color should be green.*/
 
 function toCelsius() {
- input = document.getElementById('temperature').value;
-    valNum = parseFloat(valNum);
-    document.getElementById("converted").innerHTML=(valNum-32)/1.8;
-  }
-
-
-
-function toFahrenheit () {
-
-
+    var input = document.getElementById('temperature').value;
+    input = parseFloat(input);
+    document.getElementById("converted").innerHTML = ((input-32)/1.8).toFixed(2);
+    var fahTemp = (document.getElementById("converted").innerHTML);
+    console.log(fahTemp);
+    }
+  
+function toFahrenheit() {
+  input = document.getElementById('temperature').value;
+  input = parseFloat(input);
+  document.getElementById("converted").innerHTML = (1.8 * input) + 32;
+  var celTemp = (document.getElementById("converted").innerHTML).value;
 }
 
 // Get a reference to the button element in the DOM
-var button = document.getElementById("conversionbtn").addEventListener("click", determineConverter);
+let button = document.getElementById("conversionbtn").addEventListener("click", determineConverter);
 // Assign a function to be executed when the button is clicked
 //button.addEventListener("click", determineConverter);
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
-  switch(clickEvent) {
-  case fah:
-        toFahrenheit();
-        break;
-  case cel:
-        toCelsius();
-        break;      
-  console.log("event", clickEvent);
-}
+  if(document.getElementById('celTofah').checked) {
+    //celTofah radio button is checked
+    return toFahrenheit();
+  }else if(document.getElementById('fahTocel').checked) {
+    //fahTocel radio button is checked
+    return toCelsius();
+  }
 }
 
+      const changeInputColor = () => {
+        let fahTemp = document.getElementById("converted").innerHTML;
+        console.log(fahTemp);
+        let celTemp = document.getElementById("converted").innerHTML;
+              if (fahTemp > 90 || celTemp > 32){
+            //If the temperature is greater than 90F/32C the color of the converted temperature should be red.
+              document.getElementById("converted").style.color = "red";
+           } else if(fahTemp < 32 || celTemp === 0){
+             //If the temperature is less than 32F/0C the color of the converted temperature should be blue.
+              document.getElementById("converted").style.color = "blue";
+           } else if (fahTemp < 0 || celTemp < 0){
+             // For any other temperature, the color should be green.*/
+              document.getElementById("converted").style.color = "green";
+          }
+          }
+          changeInputColor();
+
+          //Create another button that, when clicked, clears any text in the input field.
+          let resetBtn = document.getElementById("reset").addEventListener("click", resetAllField);
+
+         const resetAllFields = () => {
+            document.getElementById("resetAll").reset();
+        }
+
+        resetAllFields();
